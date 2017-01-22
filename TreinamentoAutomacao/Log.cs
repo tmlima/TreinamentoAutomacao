4c0f72b96c9fc57688188d97f5735ae3ed9a86db
tmlima <thiago.m.lima@outlook.com>
2017-01-22 12:45:05 -0200

    Exercicio2

---
+++ TreinamentoAutomacao/Log.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace TreinamentoAutomacao
+{
+    public class Log
+    {
+        private string filePath;
+        private string logContents;
+
+        public Log(string logDirectory, string fileName)
+        {
+            if ( !Directory.Exists( logDirectory ) )
+            {
+                Directory.CreateDirectory( logDirectory );
+            }
+
+            this.filePath = logDirectory + fileName;
+        }
+
+        public void Write(string message)
+        {
+            logContents += "[" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + "]" + message + Environment.NewLine;
+        }
+
+        public void Save()
+        {
+            File.WriteAllText( this.filePath + ".txt", logContents );
+        }
+    }
+}
--- TreinamentoAutomacao/UnitTest1.cs
+++ TreinamentoAutomacao/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using System.Drawing.Imaging;
 
 namespace TreinamentoAutomacao
 {
@@ -11,11 +12,31 @@ namespace TreinamentoAutomacao
         [TestMethod]
         public void TestMethod1()
         {
+            const string EvidencesDirectory = "C:\\Evidences\\";
+            string logFileName = "Test_" + GetTimestamp();
+            Log log = new Log( EvidencesDirectory, logFileName );
             ChromeDriver driver = new ChromeDriver();
+
+            log.Write( "Going to http://google.com" );
             driver.Url = "http://google.com";
+
             IWebElement searchField = driver.FindElementById( "lst-ib" );
+            log.Write( "Writing searchText" );
             searchField.SendKeys( "Automation" );
+
+            log.Write( "Clicking enter" );
             searchField.SendKeys( Keys.Enter );
+
+            log.Write( "Taking screenshot" );
+            Screenshot googleResults = driver.GetScreenshot();
+            googleResults.SaveAsFile( EvidencesDirectory + GetTimestamp() + ".jpeg", ImageFormat.Jpeg );
+            log.Save();
         }
+
+        private string GetTimestamp()
+        {
+            return DateTime.Now.ToString( "yyyyMMdd_hhmmss" );
+        }
+
     }
 }
